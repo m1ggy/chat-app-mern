1c3c41538fbc09f83280e47d5ed11ffdbb171ae0
m1ggy <46958815+m1ggy@users.noreply.github.com>
2021-08-18 16:26:41 +0800

    clean up boilerplate, added socketio for client and server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
 const express = require('express')();
+const httpServer = require('http').createServer(express);
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
+
+let connectedUsers = [];
+
+io.on('connection', (socket) => {
+  connectedUsers.push(socket.id);
+  console.log(connectedUsers);
+  socket.on('disconnect', (reason) => {
+    connectedUsers = connectedUsers.filter((user) => user != socket.id);
+    console.log(connectedUsers);
+    console.log(reason);
+  });
+});
+
+httpServer.listen(8888);
